bd4605e905c1c82d4efcc6a2f099c56e52281ebc
ppulseneon <pulseneon@vk.com>
2024-03-27 18:35:21 +0300

    add: db entities

---
+++ CFP.Domain/Entities/Activity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CFP.Domain.Entities
+{
+    public class Activity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ CFP.Domain/Entities/Application.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CFP.Domain.Entities
+{
+    public class Application
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Author { get; set; }
+        public Guid Activity { get; set; }
+        public string? Description { get; set; }
+        public string Outline { get; set; }
+    }
+}
